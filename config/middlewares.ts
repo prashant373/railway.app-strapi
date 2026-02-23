82c177bf03d2f6668fa8a38ea50386b5fc6bb783
Prashant Shrestha <prashantshrestha700@gmail.com>
2026-02-23 13:23:12 +0530

    upgrade to Strapi 5.36.1 and configure for Railway + PostgreSQL deployment

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,7 +1,31 @@
-export default [
+export default ({ env }) => [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'dl.airtable.com',
+            `res.cloudinary.com`,
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            `res.cloudinary.com`,
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
